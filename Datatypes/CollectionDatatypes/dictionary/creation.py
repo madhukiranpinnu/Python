7e2942e21909082d431e839484617bea6ee41be5
MADHUKIRAN <madhukiran918@gmail.com>
2023-08-22 22:25:07 +0530

    dictionary

---
+++ Datatypes/CollectionDatatypes/dictionary/creation.py
@@ -0,0 +1,14 @@
+d={}
+print(d)
+print(type(d))
+#multi
+d={10:"madhu",20:"sai"}
+print(d)
+print(type(d))
+#From tuple of list
+#but set of list is impossible
+#all combinations except above are possible
+#with three or more impossible
+s=([10,20],[23,45],[34,45])
+s1=dict(s)
+print(s1)
